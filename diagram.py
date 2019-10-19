5a38f2236371faf248c9ea689ad057be7bb805fa
Matthieu Blanke <matthieu@MacBook-Pro-de-Matthieu-2.local>
2019-10-19 12:51:35 +0200

    Improved prints

--- diagram.py
+++ diagram.py
@@ -16,9 +16,10 @@ class Diagram:
         self.simplices_number = len(self.simplices)
 
     def to_string(self):
+        string = 'Diagram object with following simplices :\n'
         for simplex in self.simplices:
-            print(
-                f'time = {simplex.time}, dim = {simplex.dim}, vertices = {simplex.vertices}')
+            string += f'time = {simplex.time}, dim = {simplex.dim}, vertices = {simplex.vertices}\n'
+        return string
 
     def sort_simplices(self):
         self.order_dictionary = {}
@@ -30,7 +31,7 @@ class Diagram:
 
     def build_matrix(self):
         self.matrix = [[0 for n in range(self.simplices_number)]
-                                         for m in range(self.simplices_number)]
+                       for m in range(self.simplices_number)]
         for index in range(len(self.simplices)):
             if len(self.simplices[index].vertices) == 1:
                 continue
@@ -45,22 +46,24 @@ class Diagram:
             self.pivots.append(find_pivot(self.matrix, j))
 
         for column_index in range(len(self.matrix)):
-            print(f'column index ={column_index}')
             first_occurence = self.pivots.index(self.pivots[column_index])
             while first_occurence < column_index and self.pivots[column_index] >= 0:
                 sum(self.matrix, first_occurence, column_index)
-                self.pivots[column_index] = find_pivot(self.matrix, column_index)
+                self.pivots[column_index] = find_pivot(
+                    self.matrix, column_index)
                 first_occurence = self.pivots.index(self.pivots[column_index])
 
     def build_diagram(self):
         self.diagram = []
         for index in range(self.simplices_number):
-            if self.pivots[index] < 0 :
-                try :
+            if self.pivots[index] < 0:
+                try:
                     end = self.pivots.index(index)
-                    interval = (self.simplices[index].dim,self.simplices[index].time,self.simplices[end].time)
+                    interval = (
+                        self.simplices[index].dim, self.simplices[index].time, self.simplices[end].time)
                 except ValueError:
-                    interval = (self.simplices[index].dim,self.simplices[index].time,"inf")
+                    interval = (self.simplices[index].dim,
+                                self.simplices[index].time, "inf")
                 self.diagram.append(interval)
 
     def print_diagram(self):
@@ -68,16 +71,13 @@ class Diagram:
             print("{} {} {}".format(*interval))
 
 
-
-
-
 def find_pivot(matrix, column_index):
     index = len(matrix)-1
-    while index >=0 and matrix[index][column_index] == 0:
+    while index >= 0 and matrix[index][column_index] == 0:
         index -= 1
     return index
 
+
 def sum(matrix, source, target):
     for i in range(len(matrix)):
-        matrix[i][target] = (matrix[i][target] + matrix[i][source] )%2
-
+        matrix[i][target] = (matrix[i][target] + matrix[i][source]) % 2
--- td5.py
+++ td5.py
@@ -2,9 +2,9 @@ from diagram import Diagram
 
 diagram = Diagram()
 diagram.read_data('filtration.txt')
-diagram.to_string()
+print(diagram.to_string())
 diagram.sort_simplices()
-diagram.to_string()
+print(diagram.to_string())
 diagram.build_matrix()
 diagram.reduce_matrix()
 diagram.build_diagram()
