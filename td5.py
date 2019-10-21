df2b5da671727308f4349cc5dc3db0351dd6a215
Matthieu Blanke <matthieu@MacBook-Pro-de-Matthieu-2.local>
2019-10-21 22:30:58 +0200

    sparse matrix

--- td5.py
+++ td5.py
@@ -3,19 +3,26 @@ import time
 
 start_time = time.time()
 
-# filtration_path = 'filtrations/filtration_B.txt'
 filtration_path = 'filtration.txt'
+filtration_path = 'filtrations/filtration_B.txt'
 
+use_sparse_matrix = True
+# use_sparse_matrix = False
 
-diagram = Diagram()
-print('Reading data')
+diagram = Diagram(use_sparse=use_sparse_matrix)
+print(f'Reading data from {filtration_path}')
 diagram.read_data(filtration_path)
+m = diagram.simplices_number
+print(f'Simplices count is m = {m}')
+
 print('Sorting simplices')
 diagram.sort_simplices()
+
 print('Building boundary matrix')
 matrix_start_time = time.time()
 diagram.build_matrix()
-print(f'Matrix built in {time.time() - matrix_start_time} seconds ---' )
+print(f'Matrix of size {len(diagram.matrix)} built in {time.time() - matrix_start_time} seconds ---' )
+
 
 print('Reducing boundary matrix')
 reduction_start_time = time.time()
@@ -24,8 +31,8 @@ print(f'Matrix reduced in {time.time() - reduction_start_time} seconds ---' )
 
 print('Building diagram')
 diagram.build_diagram()
-diagram.print_diagram()
-m = diagram.simplices_number
 
-print(f'Simplices count is m = {m}')
+# print(f'final pivots : {diagram.pivots}')
+diagram.print_diagram()
+
 print(f'Total execution time : {time.time() - start_time} seconds ---' )
